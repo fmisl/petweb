20699e9c9a3107d717c2bbf4df239b3be153226f
dwnusa <dwnusa@gmail.com>
2021-02-17 11:39:39 +0900

    yarn install && start by jenkins

--- petweb-front/src/App.js
+++ petweb-front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!!
+          Learn React!!!! start at 3000 port
         </a>
       </header>
     </div>
