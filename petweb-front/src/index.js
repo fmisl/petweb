c9ca58b15217ea01b2e288c47bdee5ab4bd0c331
dwnusa <dwnusa@gmail.com>
2021-02-18 21:45:01 +0900

    redux

--- petweb-front/src/index.js
+++ petweb-front/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import allReducer from './reducers';
+import {Provider} from 'react-redux';
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
@@ -15,3 +25,41 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+// //STORE -> GLOBALIZED STATE
+// // let store = createStore(reducer)
+// //ACTION INCREMENT
+// const increment = () =>{
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+// const decrement = () =>{
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// //REDUCER 
+// const counter = (state = 0, action) =>{
+//   switch(action.type){
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state -1;
+//   }
+// };
+
+// let store = createStore(counter);
+
+// //Display it in the console
+// store.subscribe(()=>console.log(store.getState()));
+
+// //DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
