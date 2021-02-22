97bbd6b047e4b13a8430141d6ae248f4f8b6fe68
dwnsnu <dwnsnu@snu.ac.kr>
2021-02-23 00:25:13 +0900

    update: nohub gunicorn

---
+++ petweb-back/gunicorn.conf.py
@@ -0,0 +1,7 @@
+# background run
+# nohup gunicorn -c gunicorn.conf.py backend.wsgi &
+bind = "0.0.0.0:8100"                   # Don't use port 80 becaue nginx occupied it already.
+errorlog = '/Users/dwnusa/workspace/SNU/log/gunicorn-error.log'  # Make sure you have the log folder create
+accesslog = '/Users/dwnusa/workspace/SNU/log/gunicorn-access.log'
+loglevel = 'debug'
+workers = 3     # the number of recommended workers is '2 * number of CPUs + 1'
