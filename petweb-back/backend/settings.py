cde9914f66e1ab2cb22c8246a2da9889a3aefea7
dwnusa <dwnusa@gmail.com>
2021-02-23 15:31:01 +0900

    update: djangorestframework auth

--- petweb-back/backend/settings.py
+++ petweb-back/backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -56,8 +58,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
--- petweb-back/backend/urls.py
+++ petweb-back/backend/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/token/', obtain_jwt_token),
+    # path('api/token/verify/', verify_jwt_token),
+    # path('api/token/refresh/', refresh_jwt_token),
+    # path('api/blog/', include('blog.urls'))
+
+    path('api-jwt-auth/', obtain_jwt_token),
+    path('api-jwt-auth/refresh/', refresh_jwt_token),
+    path('api-jwt-auth/verify/', verify_jwt_token),
 ]
