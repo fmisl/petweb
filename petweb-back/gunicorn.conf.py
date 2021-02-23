fbd267968556331d82d708343fa029ecdf7dd0d8
dwnsnu <dwnsnu@snu.ac.kr>
2021-02-23 15:37:11 +0900

    update: corsheaders

--- petweb-back/gunicorn.conf.py
+++ petweb-back/gunicorn.conf.py
@@ -1,7 +1,8 @@
-# background run
+# gunicorn --bind 0.0.0.0:8000 backend.wsgi:application
+# gunicorn -c gunicorn.conf.py backend.wsgi
 # nohup gunicorn -c gunicorn.conf.py backend.wsgi &
 bind = "0.0.0.0:8100"                   # Don't use port 80 becaue nginx occupied it already.
 errorlog = '/Users/dwnusa/workspace/SNU/log/gunicorn-error.log'  # Make sure you have the log folder create
 accesslog = '/Users/dwnusa/workspace/SNU/log/gunicorn-access.log'
 loglevel = 'debug'
-workers = 3     # the number of recommended workers is '2 * number of CPUs + 1'
+workers = 2     # the number of recommended workers is '2 * number of CPUs + 1'
