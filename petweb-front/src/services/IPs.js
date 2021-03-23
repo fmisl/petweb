00851e3559e776a3c979194babc96aecbe6c3b2e
dwnusa <dwnusa@gmail.com>
2021-03-23 15:15:57 +0900

    update: IPs globalIP

--- petweb-front/src/services/IPs.js
+++ petweb-front/src/services/IPs.js
@@ -2,4 +2,4 @@ const globalIP = "http://147.47.228.204:8002/";
 const developIP = "http://147.47.228.204:8022/"
 const localIP = "http://localhost:8022/";
 
-export const IPinUSE = developIP;
+export const IPinUSE = globalIP;
