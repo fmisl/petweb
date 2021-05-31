821c768a1190217640dea578ee4cb02157c5f543
dwnusa <dwnusa@gmail.com>
2021-05-31 09:40:15 +0900

    update: tracer

--- petweb-front/src/modal/ConnectPACS.js
+++ petweb-front/src/modal/ConnectPACS.js
@@ -191,14 +191,14 @@ function ConnectPACS({ setListID, listID, setFetchState, fetchState, selectTrace
                             </label>
                         </div>
                         {/* findHandler();  */}
-                        {stepChecker == 0 && <div className="pacs-form" style={{display: "flex", justifyContent:"flex-end", border:"0px red solid", boxSizing:"border-box"}}>
+                        {stepChecker == 0 && !fetching && <div className="pacs-form" style={{display: "flex", justifyContent:"flex-end", border:"0px red solid", boxSizing:"border-box"}}>
                             <div style={{}} className="pacs-btn" onClick={()=>{setCurrentJPGURL_head(''); findHandler();}}>Search</div>
                         </div>}
                         {/* getHandler();   */}
                         {stepChecker == 1 && <div className="pacs-form" style={{display: "flex", justifyContent:"flex-end", border:"0px red solid", boxSizing:"border-box"}}>
                             <div style={{}} className="pacs-btn type1" onClick={()=>{setCurrentJPGURL_head(''); getHandler();}}>Download</div>
                         </div>}
-                        {stepChecker == 2 && <div className="pacs-form" style={{display: "flex", justifyContent:"flex-end", border:"0px red solid", boxSizing:"border-box"}}>
+                        {stepChecker == 0 && fetching && <div className="pacs-form" style={{display: "flex", justifyContent:"flex-end", border:"0px red solid", boxSizing:"border-box"}}>
                             <div style={{}} className="pacs-btn type1" onClick={()=>{setCurrentJPGURL_head(''); }}>Loading</div>
                         </div>}
                     </div>
