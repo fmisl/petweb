484c71e4ffb5768b542a03e001b203f2f0045907
dwnusa <dwnusa@gmail.com>
2021-05-31 13:39:29 +0900

    update: tracer

--- petweb-front/src/modal/ConnectPACS.js
+++ petweb-front/src/modal/ConnectPACS.js
@@ -274,7 +274,7 @@ function ConnectPACS({ setListID, listID, setFetchState, fetchState, selectTrace
                         {fetching ? <div>{dcmCount}<img src={loadingGIF}/></div>:<PACsTable setListID={setListID} selectTracer={selectTracer} fileList={finddata} getJPGURL={getJPGURL} removeFileList={removeFileList} updateFileList={updateFileList}/>}
                     </div>}
                     {stepChecker == 2 && <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginTop:"20px", height:"70%", width:"103%", border:"0px white solid", boxSizing:"border-box"}}>
-                        {fetching ? <div>{dcmCount}<img src={loadingGIF}/></div>:<PACsTable2 setListID={setListID} selectTracer={selectTracer} fileList={getdata} getJPGURL={getJPGURL} removeFileList={removeFileList} updateFileList={updateFileList}/>}
+                        {fetching ? <div style={{border: "1px red solid"}}>{dcmCount}</div>:<PACsTable2 setListID={setListID} selectTracer={selectTracer} fileList={getdata} getJPGURL={getJPGURL} removeFileList={removeFileList} updateFileList={updateFileList}/>}
                     </div>}
                 </div>
                 <div style={{position:"relative",width:"750px",height:"100%", background:"#383C41", border:"0px red solid"}}>
